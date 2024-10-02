74e5796ba59dd7430776cb2637677e103868f21a
sahilt65 <sahiltpict@gmail.com>
2024-10-02 19:48:42 +0530

    Add Connecting and writing on Browsers get request

--- src/App.java
+++ src/App.java
@@ -1,14 +1,21 @@
 import java.net.ServerSocket;
 import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-         final ServerSocket server = new ServerSocket(8080);
+        ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080 ....");
-        while (true){
-        // spin forever
+        while (true) {
+            try (Socket socket = server.accept()) {
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today + "Sahil Tiwade";
+                socket.getOutputStream()
+                      .write(httpResponse.getBytes("UTF-8"));
+            }
         }
     }
 }
